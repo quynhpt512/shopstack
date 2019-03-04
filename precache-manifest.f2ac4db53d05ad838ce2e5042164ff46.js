6cc504270cbe89fbb187e53fab7a7bec58ce5c08
Quynh Pham <quynh.p@shopstack.asia>
2019-03-05 00:19:34 +0700

    Updates

---
+++ precache-manifest.f2ac4db53d05ad838ce2e5042164ff46.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5dfc78242b7c1f8845cd",
+    "url": "/shopstack/static/js/runtime~main.5dfc7824.js"
+  },
+  {
+    "revision": "47f74980ff1589077cd5",
+    "url": "/shopstack/static/js/main.47f74980.chunk.js"
+  },
+  {
+    "revision": "61da7be57be3a282a589",
+    "url": "/shopstack/static/js/2.61da7be5.chunk.js"
+  },
+  {
+    "revision": "47f74980ff1589077cd5",
+    "url": "/shopstack/static/css/main.be9cd952.chunk.css"
+  },
+  {
+    "revision": "459b73d3c50674a9edc8b1ad4a90c227",
+    "url": "/shopstack/index.html"
+  }
+];
